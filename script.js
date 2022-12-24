bd7bed5ec7a3d7ec507fa0b9bd8f90505dffa65c
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2022-12-24 03:39:02 +0100

    Animaciones y funcionamiento final versión móvil
    Signed-off-by: Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 const textoEntrada = document.querySelector('.texto-entrada');
 const textoSalida = document.querySelector('.texto-salida');
 const bloqueResultado = document.querySelector('.bloque');
+const noMensajeNotify = document.querySelector('.no-mensaje');
+const tieneAcentoNotify = document.querySelector('.tiene-acento');
+const textoCopiadoNotify = document.querySelector('.texto-copiado');
 
-var texto = "", temaOscuro = false, longitudInput = 28, pixelAlturaInput = 31;
+var texto = "", longitudInput = 28, pixelAlturaInput = 31;
+var tieneAcento = false, temaOscuro = false, estaMostrandoNotify = false;
 
 function encriptar(){
-    if(textoEntrada.value == "" || textoEntrada.value == null){
-        bloqueResultado.style.display = "none";
+    if(textoEntrada.value == '' || textoEntrada.value == null ){
+        mostrarNotificacion(noMensajeNotify);
+    } else if(tieneAcento){
+        mostrarNotificacion(tieneAcentoNotify);
     } else {
+        textoSalida.textContent = '';
         bloqueResultado.style.display = "block";
-
         texto = textoEntrada.value;
         texto = texto.replaceAll("e","enter");
         texto = texto.replaceAll("i","imes");
@@ -18,14 +24,19 @@ function encriptar(){
         texto = texto.replaceAll("o","ober");
         texto = texto.replaceAll("u","ufat");
 
-        textoSalida.textContent = texto;
+        setTimeout(()=>{
+            animacionEscritura(texto);
+        },200);
     }
 }
 
 function desencriptar(){
     if(textoEntrada.value == "" || textoEntrada.value == null){
-        bloqueResultado.style.display = "none";
+        mostrarNotificacion(noMensajeNotify);
+    } else if(tieneAcento){
+        mostrarNotificacion(tieneAcentoNotify);
     } else {
+        textoSalida.textContent = '';
         bloqueResultado.style.display = "block";
         texto = textoEntrada.value;
         texto = texto.replaceAll("ai","a");
@@ -34,40 +45,35 @@ function desencriptar(){
         texto = texto.replaceAll("ober","o");
         texto = texto.replaceAll("ufat","u");
 
-        textoSalida.textContent = texto;
+        setTimeout(()=>{
+            animacionEscritura(texto);
+        },200);
     }
+}
+
+function soloLetras(texto){
+    return texto ? !/[^a-z\sñ]/.test(texto) : true;
 }
 
 function validar(e){
 
     textoEntrada.value = textoEntrada.value.toLowerCase();
+    texto = textoEntrada.value;
 
-    let key = e.keyCode || e.which;
-    let especiales="8-37-38-46-164";
-    let teclaEspeciales = false;
-
-    for (var i in especiales){
-        if (key == especiales[i]){
-            teclaEspeciales = true;
-            break;
+    if(!soloLetras(texto)){
+        tieneAcento = true;
+        document.querySelector('.reglas').style.animation = 'shake-horizontal 0.8s ease-in-out both';
+        document.querySelector('.reglas').style.color = "#e93f3f";
+    } else {
+        tieneAcento = false;
+        document.querySelector('.reglas').style.animation = '';
+        if(temaOscuro){
+            document.querySelector('.reglas').style.color = "#fafbff";
+        } else {
+            document.querySelector('.reglas').style.color = "#073481";
         }
     }
-    texto = textoEntrada.value;
-    if(texto.match('[á,é,í,ó,ú]') != null && !teclaEspeciales){
-        // advertir
-        textoEntrada.value = texto.substring(0, texto.length() - 1);
-    }   
-
-    if(texto.length >= longitudInput){
-        longitudInput += 28;
-        pixelAlturaInput += 31;
-        textoEntrada.style.height = pixelAlturaInput+"px";
-    }
-    if(texto.length + 28 < longitudInput){
-        longitudInput -= 28;
-        pixelAlturaInput -= 31;
-        textoEntrada.style.height = pixelAlturaInput+"px";
-    }
+    comprobarTamañoInput();
 }
 
 function copiar(){
@@ -118,37 +124,108 @@ function copiar(){
     })(window, document, navigator);
     Clipboard.copy(texto);
 
-    bloqueResultado.style.display = "none";
+    cerrarBloque();
+    mostrarNotificacion(textoCopiadoNotify);
 }
 
 function cambiarTema(){
     if(temaOscuro){
         // Pasar a Tema Claro
-        document.querySelector('.reglas').querySelector('p').style.color = "#073481";
-        document.querySelector('.reglas').querySelector('img').src = "img/exclamation-dark.svg";
+        temaOscuro = false;
+        // - fondo
         document.body.style.backgroundColor = "#fafbff";
-        textoEntrada.style.color = "#073481";
+        // - texto entrada y su placeholder
+        textoEntrada.style.color = "#2e2e2e";
         textoEntrada.classList.remove("texto-entrada-light");
         textoEntrada.classList.add("texto-entrada-dark");
-        document.querySelector('footer').querySelector('span').style.color = "#2e2e2e";
-        document.querySelector('.icon-linkedin').src = "img/icon-linkedin-dark.svg";
-        document.querySelector('.icon-github').src = "img/icon-github-dark.svg";
+        // - reglas
+        document.querySelector('.reglas').style.color = "#073481";
+        // - bloque
         bloqueResultado.style.backgroundColor = "white";
         textoSalida.style.color = "#2e2e2e";
-        temaOscuro = false;
+        // - footer
+        document.querySelector('footer').querySelector('span').style.color = "#2e2e2e";
+        document.querySelector('.fa-github').style.color = "#2e2e2e";
+        document.querySelector('.fa-linkedin').style.color = "#2e2e2e";
     } else {
         // Pasar a Tema Oscuro
-        document.querySelector('.reglas').querySelector('p').style.color = "#fafbff";
-        document.querySelector('.reglas').querySelector('img').src = "img/exclamation-light.svg";
+        temaOscuro = true;
+        // - fondo
         document.body.style.backgroundColor = "#1A1A1A";
+        // - texto entrada y su placeholder
         textoEntrada.style.color = "#fafbff";
         textoEntrada.classList.remove("texto-entrada-dark");
         textoEntrada.classList.add("texto-entrada-light");
-        document.querySelector('footer').querySelector('span').style.color = "#fafbff";
-        document.querySelector('.icon-linkedin').src = "img/icon-linkedin-light.svg";
-        document.querySelector('.icon-github').src = "img/icon-github-light.svg";
+        // - reglas
+        document.querySelector('.reglas').style.color = "#fafbff";
+        // - bloque
         bloqueResultado.style.backgroundColor = "#454545";
         textoSalida.style.color = "#fafbff";
-        temaOscuro = true;
+        // - footer
+        document.querySelector('footer').querySelector('span').style.color = "#fafbff";
+        document.querySelector('.fa-github').style.color = "#fafbff";
+        document.querySelector('.fa-linkedin').style.color = "#fafbff";
+    }
+
+    if(tieneAcento) {
+        document.querySelector('.reglas').style.color = "#e93f3f";
     }
 }
+
+function cerrarBloque(){
+    bloqueResultado.style.animation = 'cerrar-bloque 0.3s ease both';
+    document.querySelector('.contenedor-bloque').style.backgroundColor = '#2e2e2e00';
+    setTimeout(()=>{
+        bloqueResultado.style.animation = '';
+        bloqueResultado.style.display = 'none';
+    },300);
+}
+
+function animacionEscritura(texto) {
+    textoSalida.innerText = '';
+    let pos = 0;
+    let final = texto.length - 1;
+    const interval = setInterval(() => {
+      if(pos <= final) {
+        if(texto[pos] == ' '){
+            textoSalida.innerText += '\u00A0';
+        } else {
+            textoSalida.innerText += texto[pos];
+        }
+        pos++
+      } else {
+        clearInterval(interval);
+      }
+    }, 30);
+  }
+
+function mostrarNotificacion(notify){
+    if(!estaMostrandoNotify){
+        estaMostrandoNotify = true;
+        notify.style.display = 'flex';
+        notify.style.animation = 'notificar-entrada 0.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) both';
+        setTimeout(()=>{
+            notify.style.animation = 'notificar-salida 0.5s cubic-bezier(0.550, 0.055, 0.675, 0.190) both';
+        }, 2000);
+        setTimeout(()=>{
+            notify.style.animation = '';
+            notify.style.display = 'none';
+            estaMostrandoNotify = false;
+        }, 2500);
+    }
+}
+
+function comprobarTamañoInput(){
+    while(texto.length + 28 < longitudInput){
+        longitudInput -= 28;
+        pixelAlturaInput -= 31;
+        textoEntrada.style.height = pixelAlturaInput+"px";
+    }
+    while(texto.length >= longitudInput){
+        longitudInput += 28;
+        pixelAlturaInput += 31;
+        textoEntrada.style.height = pixelAlturaInput+"px";
+    }
+}
+
+textoEntrada.addEventListener('input', validar);
